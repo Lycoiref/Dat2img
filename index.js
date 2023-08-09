ed1bed1710f50688ff6de6a1972534cae27c46a0
Lycoiref <1808347356@qq.com>
2023-08-09 20:49:41 +0800

    完成了byFile和byPath的解析方式

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 import { log } from 'console'
-import { FileTypes } from './FileTypes.js'
+import { FileTypes } from './src/FileTypes.js'
 import fs from 'fs'
 import path from 'path'
 
-const __filename = path.resolve()
-const __dirname = path.resolve(__filename, './')
-
-// 读取.dat二进制文件
-const dat = fs.readFileSync(path.resolve(__dirname, './test_data/Image/2023-07/test.dat'))
-// 取前四位
-const header = dat[0]
-const next = dat[1]
-
-// 打印FileTypes的16进制
-Object.keys(FileTypes).forEach((item, index) => {
-    let v = FileTypes[item][0] ^ header
-    let verify = v ^ next
-    if (verify === FileTypes[item][1]) {
-        log('File type is: ', item)
-        // 对剩余所有字节进行异或解码
-        let result = dat.map((item, index) => {
-            return item ^ v
-        })
-        // 保存解码后的文件
-        fs.writeFileSync(path.resolve(__dirname, `./test_data/Image/2023-07/test.${item}`), Buffer.from(result))
+export default class datToImg {
+    constructor() {
+        this.__filename = path.resolve()
+        this.__dirname = path.resolve(this.__filename, './')
     }
-})
+    byFile(filepath, outputDir) {
+        // 读取.dat二进制文件
+        const dat = fs.readFileSync(path.resolve(this.__dirname, filepath))
+        // 取前四位
+        const header = dat[0]
+        const next = dat[1]
+
+        // 打印FileTypes的16进制
+        Object.keys(FileTypes).forEach((item, index) => {
+            let v = FileTypes[item][0] ^ header
+            let verify = v ^ next
+            if (verify === FileTypes[item][1]) {
+                log(`Translating dat file to ${item}`)
+                // 对剩余所有字节进行异或解码
+                let result = dat.map((item, index) => {
+                    return item ^ v
+                })
+                // 检查是否存在images文件夹
+                if (!fs.existsSync(path.resolve(this.__dirname, './images'))) {
+                    fs.mkdirSync(path.resolve(this.__dirname, './images'))
+                }
+                // 检查是否存在outputDir文件夹
+                if (outputDir && !fs.existsSync(path.resolve(this.__dirname, `./images/${outputDir}`))) {
+                    fs.mkdirSync(path.resolve(this.__dirname, `./images/${outputDir}`))
+                }
+                let filename = filepath.split('/').pop().split('.')[0]
+                // 保存解码后的文件
+                fs.writeFileSync(
+                    path.resolve(this.__dirname, `./images${outputDir ? `/${outputDir}/` : '/'}${filename}.${item}`),
+                    Buffer.from(result)
+                )
+            }
+        })
+    }
+    byDirectory(dirpath, outputDir) {
+        // 读取目录下所有文件
+        const files = fs.readdirSync(path.resolve(this.__dirname, dirpath))
+        // 遍历所有文件
+        files.forEach((item, index) => {
+            // 判断是否是dat文件
+            if (!item.endsWith('.dat')) {
+                return
+            }
+            this.byFile(`${dirpath}/${item}`, outputDir)
+        })
+    }
+}
